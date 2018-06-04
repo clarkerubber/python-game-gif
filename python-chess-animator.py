3f2483c65da40b9d89ced1051416aa653e2de449
clarkerubber <jamesjosephclarke@gmail.com>
2018-06-04 18:08:49 +1000

    images are now served via flask

--- python-chess-animator.py
+++ python-chess-animator.py
@@ -1,7 +1,11 @@
+from tempfile import TemporaryFile
+
 from chess.pgn import read_game
 import chess.svg
 
 from pprint import pprint
+
+from flask import Flask, send_file
 
 import requests
 import argparse
@@ -13,32 +17,39 @@ try:
 except ImportError:
     from io import StringIO
 
-#read_game(StringIO(" ".join(game.pgn)))
 
 parser = argparse.ArgumentParser(description=__doc__)
 
-parser.add_argument("--gameid", dest="gameid",
-                    help="game to animate", type=str)
-parser.add_argument("--black", dest="black",nargs='?',
-                    default=False,const=True,
-                    help="which side to view the game from")
 parser.add_argument("--css", dest="css", type=argparse.FileType("r"), default="default.css")
-parser.add_argument("--output", dest="output", type=str)
 
 settings = parser.parse_args()
 
-result = requests.get(f'https://lichess.org/game/export/{settings.gameid}')
+app = Flask(__name__)
 
 style = settings.css.read() if settings.css else None
 
-game = read_game(StringIO(result.text))
-size = 360
 
-with imageio.get_writer(settings.output, mode='I', fps=1) as writer:
-    node = game
-    while not node.is_end():
-        nextNode = node.variation(0)
-        board_svg = chess.svg.board(node.board(), coordinates=False, flipped=settings.black, size=size, style=style)
-        board_png = imageio.imread(cairosvg.svg2png(bytestring=board_svg))
-        writer.append_data(board_png)
-        node = nextNode
+@app.route('/<gameid>.gif', methods=['GET'])
+def serve_gif(gameid):
+    result = requests.get(f'https://lichess.org/game/export/{gameid}')
+
+    game = read_game(StringIO(result.text))
+    size = 360
+    tempfile = TemporaryFile()
+
+    with imageio.get_writer(tempfile, mode='I', format='gif', fps=1) as writer:
+        node = game
+        while not node.is_end():
+            nextNode = node.variation(0)
+            board_svg = chess.svg.board(node.board(), coordinates=False, flipped=False, size=size, style=style)
+            board_png = imageio.imread(cairosvg.svg2png(bytestring=board_svg))
+            writer.append_data(board_png)
+            node = nextNode
+
+    tempfile.seek(0)
+    
+    return send_file(tempfile, mimetype='image/gif')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
